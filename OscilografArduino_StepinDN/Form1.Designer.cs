04cb6da8317c514b8a7cc84703de00fd85eafe5c
unknown <Don.71rus@mail.ru>
2020-01-24 22:37:11 +0300

    Finish

---
+++ OscilografArduino_StepinDN/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace OscilografArduino_StepinDN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(406, 560);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(406, 116);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(697, 520);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 21);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Disconnect";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(271, 653);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(403, 520);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 15;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(271, 604);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "test_signal";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 512);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(834, 503);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonRun.Enabled = false;
+            this.buttonRun.Location = new System.Drawing.Point(271, 558);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 12;
+            this.buttonRun.Text = "Start/Stop";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.buttonDisconnect);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.buttonConnect);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(47, 528);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(208, 148);
+            this.panel1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "74880",
+            "115200",
+            "230400",
+            "250000"});
+            this.comboBox2.Location = new System.Drawing.Point(15, 14);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(57, 21);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Text = "9600";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(57, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "мс   Интервал";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "100";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Enabled = false;
+            this.buttonDisconnect.Location = new System.Drawing.Point(104, 115);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 5;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Имя порта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "бод   Скорость";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(14, 115);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 688);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Oscilograf";
+            this.Load += new System.EventHandler(this.Oscilograf);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OscilografArduino_StepinDN/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace OscilografArduino_StepinDN
+{
+    public partial class Form1 : Form
+    {
+        SerialPort myPort;
+        double u = 0;
+        private int width = 765, wigth_sum = 100, height = 465, viewX = 0, viewY = 0, scale = 40;
+        private double graphBegin = 0, graphEnd = 10, graphStep = 0.01;
+        private List<PointF> listPoint = new List<PointF>() { };
+        private Point clipPosition;
+        private bool clip, start = false;
+        private double time = 0;
+        //-------задающие воздействие--------------
+        private double f(double x)
+        {
+            if (checkBox1.Checked == true)
+            { return -Math.Cos(x); }
+            return u;
+        }
+        //-------------------------------------------
+        double perevod(string f)
+        {
+            double e;
+            string[] b = f.Split(new Char[] { '.' });
+            string t;
+            try
+            {
+                t = b[0] + "," + b[1];
+                e = Convert.ToDouble(t);
+                return e;
+            }
+            catch (Exception rt)
+            {
+                return 0.0;
+            }
+        }
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }     
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            int speed = Convert.ToInt32(comboBox2.Text.ToString());
+            string name = comboBox1.Text.ToString();
+
+            try
+            {
+                timer1.Interval = Convert.ToInt32(textBox1.Text.ToString());
+
+                myPort = new SerialPort(name, speed, Parity.None, 8, StopBits.One);
+                myPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                myPort.Open();
+                //label5.Text = myPort.ReadLine().ToString();
+                buttonConnect.Enabled = false;
+                buttonRun.Enabled = true;
+                buttonDisconnect.Enabled = true;
+                button1.Enabled = true;
+                MessageBox.Show("Успешное соединением с " + name);
+                label7.Text = "Connect";
+                label7.ForeColor = Color.Blue;
+            }
+            catch (Exception o)
+            {
+                MessageBox.Show("Проблема с соединением " + name);
+                MessageBox.Show(o.Message);
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Enabled = false;
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+                buttonRun.Enabled = false;
+                button1.Enabled = false;
+
+                myPort.Close();
+                myPort.Dispose();
+                // PCI.PCIDeviceClose();
+                label5.Text = "";
+                label7.Text = "Disconnect";
+                label7.ForeColor = Color.Red;
+            }
+            catch (Exception w)
+            {
+                MessageBox.Show(w.Message);
+            }
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true) { timer1.Enabled = false; start = true; }
+            else if (timer1.Enabled == false) { timer1.Enabled = true; start = true; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //---------------------------------------------------------- 
+            label5.Text = (-u).ToString(); //myPort.ReadLine().ToString();
+            //------------------------------------------------------------
+
+            Invalidate(true);
+            time += (double)timer1.Interval / 1000;                   // интервал времени ----------------при изменеии парметров таймера учитывапть эту функцию
+
+
+            double my = f(time);          //применение функции с входным воздействием 
+            viewX = -(int)(time * scale) + 50; //следование за построеной функцией по оси Х
+            // viewY = -(int)(my) * scale;   //следование за построеной функцией по оси Y
+
+            listPoint.Add(new PointF((float)(time * scale), (float)(my * scale)));
+            label4.Text = "Interval: " + timer1.Interval + "ms;   " + Math.Round(time, 3).ToString() + " c";
+            if (listPoint.Count == 3000)  //обновление каждые 300с (5 мин) //  5мин*60с*10=3000
+            {
+                listPoint.Clear();
+                wigth_sum = 100;
+                time = 0;
+            }
+
+            if (width + wigth_sum <= time * 10 * 4) //--расширение построения сетки 
+            {
+                wigth_sum += 700;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(width / 2 + viewX, height / 2 + viewY);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            for (int i = (int)scale; i < width * 2 + wigth_sum; i += (int)scale)
+            {
+                e.Graphics.DrawLine(Pens.LightGray, i, -height * 2, i, height * 2);
+                e.Graphics.DrawLine(Pens.LightGray, -i, -height * 2, -i, height * 2);
+
+                e.Graphics.DrawLine(Pens.LightGray, -width * 2, i, width * 2 + wigth_sum, i);
+                e.Graphics.DrawLine(Pens.LightGray, -width * 2, -i, width * 2 + wigth_sum, -i);
+
+                e.Graphics.DrawString((i / scale).ToString(), DefaultFont, Brushes.Black, i, 0);
+                e.Graphics.DrawString((-i / scale).ToString(), DefaultFont, Brushes.Black, -i, 0);
+            }
+            /*for (int i = (int)scale; i < height * 2; i += (int)scale)
+            {
+                e.Graphics.DrawString((i / scale).ToString(), DefaultFont, Brushes.Black, 0, -i);
+                e.Graphics.DrawString((-i / scale).ToString(), DefaultFont, Brushes.Black, 0, i);
+            }*/
+            //скользащая нумерация
+            if (start)
+            {
+                for (int i = (int)scale; i < height * 2; i += (int)scale)
+                {
+                    e.Graphics.DrawString((i / scale).ToString(), DefaultFont, Brushes.Black, -viewX, -i);
+                    e.Graphics.DrawString((-i / scale).ToString(), DefaultFont, Brushes.Black, -viewX, i);
+                }
+                //скользящая ось абцис
+                e.Graphics.DrawLine(Pens.Gray, -viewX, -height * 2, -viewX, height * 2);
+                /*  var temp = listPoint.Where(p => p.X == (double)(-viewX / scale) - 0.3 || p.X <= (double)(-viewX / scale) + 0.3);
+                  foreach (var i in temp)
+                  {
+                      label5.Text = (-i.Y/scale).ToString();
+                  }*/
+                // label5.Text = ((double)-viewX / scale).ToString();
+            }
+            e.Graphics.DrawLine(Pens.Gray, -width * 2, 0, width * 2 + wigth_sum, 0);
+            e.Graphics.DrawLine(Pens.LightGray, 0, -height * 2, 0, height * 2);
+
+
+
+            //--------------func*-----------------
+
+            for (int i = 1; i < listPoint.Count; i++)
+            {
+                e.Graphics.DrawLine(Pens.Red, listPoint[i - 1], listPoint[i]);
+            }
+
+            //------------------------------------   
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            clip = true;
+            clipPosition = new Point(e.X - viewX, e.Y - viewY);
+            Invalidate(true); 
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clip)
+            {
+                viewX = e.X - clipPosition.X;
+                viewY = e.Y - clipPosition.Y;
+                Invalidate(true);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            clip = false; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listPoint.Clear();
+            wigth_sum = 100;
+            time = 0;
+            label4.Text = "";
+            viewX = -(int)(time * scale); //следование за построеной функцией по оси Х
+            // viewY = -(int) scale;   //следование за построеной функцией по оси Y
+            label5.Text = "";
+            Invalidate(true);
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            width = pictureBox1.Width;
+            height = pictureBox1.Height;
+            Invalidate(true);
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            SerialPort sp = (SerialPort)sender;
+
+            //string msg = sp.ReadExisting();
+            try
+            {
+                string w = sp.ReadLine();
+                if (w != String.Empty)
+                {
+                    BeginInvoke(new Action(() => richTextBox1.AppendText(w)));
+
+                    u = -perevod(w);
+                    //  u = -Convert.ToDouble(w.Replace('.', ','));
+                }
+            }
+            catch (Exception t)
+            {
+                MessageBox.Show(t.Message);
+            }
+
+
+        }
+
+        private void Oscilograf(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+    }
+}
